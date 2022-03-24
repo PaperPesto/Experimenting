3dcb41500e5c0c20aa00a871c687ea64e796629d
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2022-03-24 17:42:27 +0100

    finished section 1

--- Experimenting/Assets/Driver.cs
+++ Experimenting/Assets/Driver.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] public float steerSpeed = 200f;
-    [SerializeField] public float MoveSpeed = 10f;
-    void Start()
-    {
-        Debug.Log("I'm printing to the console");
-        
-    }
+    [SerializeField] public float moveSpeed = 10f;
+    [SerializeField] public float slowSpeed = 5f;
+    [SerializeField] public float boostSpeed = 15f;
 
     // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Boost"){
+            moveSpeed = boostSpeed;
+        }
     }
 }
